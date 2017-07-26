91ab1d23f87e35ec490c8aa38ba0f8226e418912
muraleo <muraleo80923@gmail.com>
2017-07-25 21:41:28 -0400

    Stack implement by array

---
+++ cp6/src/ArrayStack.java
@@ -0,0 +1,38 @@
+public class ArrayStack<E> implements Stack<E>{
+	//instance variable	
+	public static final int CAPACITY = 1000;
+	private E[] data;
+	private int t = -1;
+
+	//constructors
+	public ArrayStack(){this(CAPACITY);}
+	public ArrayStack(int capacity){
+		data = (E[]) new Object[capacity];
+	}
+
+	//access method
+	public int size(){return t+1;}
+	public boolean isEmpty(){return t==(-1);}
+	public void push(E e){
+		data[++t] = e;
+	}
+	public E pop(){
+		if(isEmpty()) return null;
+ 		return data[t--];
+	}
+	public E top(){
+		if(isEmpty()) return null;
+		return data[t];
+	}
+
+	public static void main(String[] args){
+		//java generic cannot use primitive type, need to replace 
+		//by corresponding wrapper object class
+		ArrayStack<Integer> as = new ArrayStack<Integer>();
+		as.push(1);
+		as.push(2);
+		System.out.println(as.pop());
+		System.out.println(as.pop());
+		System.out.println(as.top());
+	}
+}
